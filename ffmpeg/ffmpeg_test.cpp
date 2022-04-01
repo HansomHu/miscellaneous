2bfeac4abc613d51379467ac11021ea1f1530a6e
huyuexiang <huyuexiang@kuaishou.com>
2022-04-01 23:47:43 +0800

    Add network test code

--- ffmpeg/ffmpeg_test.cpp
+++ ffmpeg/ffmpeg_test.cpp
@@ -12,12 +12,24 @@ extern "C" {
 #endif
 
 const std::string videoPath = "../../ffmpeg/test.mp4";
+const std::string rtmpUrl = "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear2/prog_index.m3u8";
+
 int main(int argc, char** argv) {
     std::cout << avcodec_configuration() << std::endl;
     std::cout << "Working directory: " << getcwd(nullptr, 0) << std::endl;
     av_register_all();
     AVFormatContext* fmtCtx = nullptr;
+#if 0
+    // Open network media
+    avformat_network_init(); // Don't forget avformat_network_deinit() later
+    AVDictionary* opt = nullptr;
+    av_dict_set(&opt, "rtsp_transport", "tcp", 0);
+    av_dict_set(&opt, "max_delay", "550", 0);
+    auto ret = avformat_open_input(&fmtCtx, rtmpUrl.c_str(), nullptr, &opt);
+#else
+    // Open local media
     auto ret = avformat_open_input(&fmtCtx, videoPath.c_str(), nullptr, nullptr);
+#endif
     if (ret != 0) {
         char data[512];
         av_strerror(ret, data, 512);
